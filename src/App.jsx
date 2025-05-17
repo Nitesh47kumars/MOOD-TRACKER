723eedee95221e9bbfcc1f309415b6e0bb796efc
Nitesh47kumars <niteshshah028@gmail.com>
2025-05-17 15:50:54 +0530

    Add Navbar component with dark/light mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import {MoodSelector} from './Components/MoodSelector'
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
 const App = () => {
   const [selectMood,setSelectMood] = useState("");
+  const [mode,setMode] = useState(false);
   return (
     <>
-      <div className='container-fluid'>
-        <div className='container d-flex justify-content-center'>
-          <MoodSelector selectedMood={selectMood} onSelectMood={setSelectMood} />
-        </div>
-      </div>
+      <Router>
+        <Navbar mode={mode} setMode={setMode} />
+          <Routes>
+            <Route path='/' element={<MoodSelector selectedMood={selectMood} onSelectMood={setSelectMood} />} />
+          </Routes>
+      </Router>
     </>
   )
 }
